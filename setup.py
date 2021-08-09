67082f86b4795cb12369a934c07f82e189cdb9a3
waldemar <hellodoge@tuta.io>
2021-08-09 15:19:38 +0300

    add console entry_point to setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,11 @@ setuptools.setup(
     name='deephide',
     description='Python tool to hide information in media files',
     author='waldemar',
-    author_email='helldoge@tuta.io'
+    author_email='helldoge@tuta.io',
+    packages=['deephide'],
+    entry_points={
+        'console_scripts': [
+            'deephide = deephide.__main__:main',
+        ],
+    },
 )
